8bde976584c8f6e0ede521e2d447adcfbbba2780
Adam <zsoriadam10@gmail.com>
2019-02-21 19:49:36 +0100

    vmi

--- pentimum/fordito/Program.cs
+++ pentimum/fordito/Program.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace fordito
 {
     class Program
     {
         static void Main(string[] args) {
+            List<string> sorelemek = new List<string>();
+            using (StreamReader sr = new StreamReader("1pelda.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    sorelemek=new List<string>();
+                    if(sor[0]!='#')
+                    {
+                        string[] elemek =sor.Split(' ');
+                    }
+                }
+
+            }
         }
     }
 }
